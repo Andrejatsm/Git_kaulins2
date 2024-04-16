602b1cff363b72c9150b0a71f9297ad2a244b400
Račkovs <andrejs.rackovs@sk.lvt.lv>
2024-04-16 10:35:51 +0300

    Es vēlos nomirt(metode)

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,12 +2,13 @@ import java.util.Random;
 
 public class Kaulins {
 	 
-	 public static void main(String[] args) {
-		 int skaitlis;
+	static void mestKaulinu() {
+		int skaitlis;
 		 Random rand = new Random();
 		 skaitlis = rand.nextInt(6)+1;
 		 System.out.println("Uzkritis skaitlis: "+skaitlis);
-		 System.out.println("Jauna rinda");
-		 
+	}
+	 public static void main(String[] args) {
+ mestKaulinu();
 	 }
  }
